2eec827bcc046edb8ea6a6eabfa14a6d25564185
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-22 11:31:52 -0400

    TMC ProjecTMC Projects MOOC.fi

---
+++ 2013-OOProgrammingWithJava-PART1/week1-003.Spruce/src/Spruce.java
@@ -0,0 +1,9 @@
+public class Spruce {
+
+    public static void main(String[] args) {
+
+        // Write code here
+        System.out.println("Hello world!\n(And all the people of the world)");
+    }
+
+}
