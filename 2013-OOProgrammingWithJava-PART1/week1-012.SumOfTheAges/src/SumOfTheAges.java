65f7cd2294316b5f10c050590c7e712cd3bc7a9a
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-22 12:04:32 -0400

    Even more

--- 2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ 2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class SumOfTheAges {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Type your name: ");
+        String name1 = in.nextLine();
+        System.out.println("Type your age: ");
+        int a = Integer.parseInt(in.nextLine());
+        System.out.println("\nType your name: ");
+        String name2 = in.nextLine();
+        System.out.println("Type your age: ");
+        int b = Integer.parseInt(in.nextLine());
         // Implement your program here
+        int totage = a + b;
+        System.out.println(name1 + " and " + name2 + " are " + totage + " years old in total.");
     }
 }
--- 2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
@@ -4,5 +4,20 @@ import nhlstats.NHLStatistics;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Top ten by goals");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+        
+        System.out.println("\nTop ten by penalties");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+        
+        System.out.println("");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+        System.out.println("");
+        NHLStatistics.teamStatistics("PHI");
+        System.out.println("");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
     }
 }
