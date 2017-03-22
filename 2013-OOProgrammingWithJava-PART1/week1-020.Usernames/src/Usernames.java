a88aac2eee6f0d58e95c3feda0cbf2f3de5f3a70
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-22 18:35:52 -0400

    Added solutions

--- 2013-OOProgrammingWithJava-PART1/week1-020.Usernames/src/Usernames.java
+++ 2013-OOProgrammingWithJava-PART1/week1-020.Usernames/src/Usernames.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class Usernames {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type your username: ");
+        String user = in.nextLine();
+        System.out.println("Type your password: ");
+        String pass = in.nextLine();
+        if ((user.equals("alex") && pass.equals("mightyducks")) || (user.equals("emily") && pass.equals("cat"))) {
+            System.out.println("You are now logged into the system!");
+        } else {
+            System.out.println("Your username or password was invalid!");
+        }
     }
 }
--- 2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
+++ 2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class LeapYear {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type a year: ");
+        int a = Integer.parseInt(in.nextLine());
+        if (a % 4 == 0) {
+            if (a % 100 == 0 && a % 400 != 0) {
+                System.out.println("The year is not a leap year.");
+            } else {
+                System.out.println("The year is a leap year.");
+            }    
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
