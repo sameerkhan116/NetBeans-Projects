48ab1ecd0b042c94ad5365371d5e880447d88266
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-23 17:15:54 -0400

    Last for today

--- 2013-OOProgrammingWithJava-PART1/week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ 2013-OOProgrammingWithJava-PART1/week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -2,18 +2,43 @@ public class PrintingLikeBoss {
 
     // copy or rewrite the method of Assignment 39.1 here
     public static void printStars(int amount) {
+        int i = 0;
+        while (i < amount) {
+            System.out.print("*");
+            i++;
+        }
+        System.out.println();
     }
 
     public static void printWhitespaces(int amount) {
-        // 40.1
+        while (amount > 0) {
+            System.out.print(" ");
+            amount--;
+        }
     }
 
     public static void printTriangle(int size) {
-        // 40.2
+        int i = 1;
+        while (i <= size) {
+            printWhitespaces(size-i);
+            printStars(i);
+            i++;
+        }
     }
 
     public static void xmasTree(int height) {
         // 40.3
+        int i = 1, a = 1;
+        while (i <= height) {
+            printWhitespaces(height-i);
+            printStars(a);
+            a += 2;
+            i++;
+        }
+        for(int x = 0; x < 2; x++) {
+            printWhitespaces(height - 2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
