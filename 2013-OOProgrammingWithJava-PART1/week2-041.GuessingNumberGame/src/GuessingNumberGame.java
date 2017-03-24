fa54e8da0cc53786b58db6f52962efa163472da0
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-24 02:43:18 -0400

    Week 2 completed

--- 2013-OOProgrammingWithJava-PART1/week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ 2013-OOProgrammingWithJava-PART1/week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -7,9 +7,22 @@ public class GuessingNumberGame {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
-
+        int a, count = 0;
         // program your solution here. Do not touch the above lines!
-        
+        while (true) {
+            System.out.println("Guess a number: ");
+            a = Integer.parseInt(reader.nextLine());
+            if (a < numberDrawn) {
+                count++;
+                System.out.println("The number is greater, guesses made: " + count);
+            } else if (a > numberDrawn) {
+                count++;
+                System.out.println("The number is lesser, guesses made: " + count);
+            } else if (a == numberDrawn) {
+                System.out.println("Congratulations, your guess is correct!");
+                break;
+            }
+        }
     }
 
     // DO NOT MODIFY THIS!
--- 2013-OOProgrammingWithJava-PART1/week2-042.Hangman/src/HangmanUserInteface.java
+++ 2013-OOProgrammingWithJava-PART1/week2-042.Hangman/src/HangmanUserInteface.java
@@ -16,8 +16,21 @@ public class HangmanUserInteface {
         System.out.println("");
 
         // PROGRAM YOUR SOLUTION HERE
-
-
+        while(hangman.gameOn()) {
+            System.out.println("Type a command: ");
+            String command = reader.nextLine();
+            if (command.equals("quit")) {
+                break;
+            } else if (command.equals("status")) {
+                hangman.printStatus();
+            } else if (command.length() == 1) {
+                hangman.guess(command);
+            } else if (command.equals("")) {
+                printMenu();
+            }
+            hangman.printMan();
+            hangman.printWord();
+        }
         System.out.println("Thank you for playing!");
     }
 
