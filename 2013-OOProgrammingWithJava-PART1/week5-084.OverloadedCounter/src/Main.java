df390765768c3b9ce8cc4f8f0d2eb65df5073cb0
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-28 10:18:46 -0400

    84 done

--- 2013-OOProgrammingWithJava-PART1/week5-084.OverloadedCounter/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week5-084.OverloadedCounter/src/Main.java
@@ -1,5 +1,17 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write testcode here
+        Counter counter = new Counter(10, true);
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("starting value: ");
+//        int x = Integer.parseInt(reader.nextLine());
+//        System.out.println("Check: ");
+//        boolean a = reader.nextBoolean();
+        counter.decrease();
+        
+        
     }
 }
