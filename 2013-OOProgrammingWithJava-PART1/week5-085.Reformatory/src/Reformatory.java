febdb8d5f792f120a36cf64d31638dd6324eb23c
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-29 09:51:36 -0400

    Reformatory (ex. 85 completed)

--- 2013-OOProgrammingWithJava-PART1/week5-085.Reformatory/src/Reformatory.java
+++ 2013-OOProgrammingWithJava-PART1/week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,17 @@
 public class Reformatory {
-
+    private int count = 0;
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        this.count++;
+        return person.getWeight();
     }
-
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int totalWeightsMeasured() {
+        return this.count;
+    }
+ 
 }
