cfb2caf0c4b6fd4b901e345d4c3dac81f2ec39fa
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-30 22:45:19 -0400

    Another one

--- 2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
@@ -19,9 +19,20 @@ public class Main {
             }
         }
         
-        for(Student pers: arrays) {
+        
+        for(Student pers : arrays) {
              System.out.println(pers);
         }
+        
+        System.out.println("Given search term: ");
+        String search = reader.nextLine();
+        System.out.println("Result:");
+        for(Student pers : arrays) {
+            if(pers.getName().contains(search)) {
+                System.out.println(pers.toString());
+            }
+        }
+       
        
     }
 }
