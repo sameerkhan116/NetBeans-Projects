7621ed6906225976881902dbd925d5dfdabd2693
Sameer Khan <sameer_khan@student.uml.edu>
2017-04-07 12:59:12 -0400

    98 done

--- 2013-OOProgrammingWithJava-PART1/week6-098.CopyAndReverse/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,35 @@
 
+import java.util.Arrays;
+
+
 
 public class Main {
 
+    public static int[] copy(int[] array) {
+        int[] newArr = new int[array.length];
+        
+        for (int i = 0; i < array.length; i++) {
+            newArr[i] = array[i];
+        }
+        return newArr;
+    }
+    
+    public static int[] reverseCopy(int[] array) {
+        int[] newArr = new int[array.length];
+        
+        for(int i = 0; i < array.length; i++) {
+            newArr[i] = array[array.length - i - 1];
+        }
+        return newArr;
+    }
+    
     public static void main(String[] args) {
-        // write testcode here
+        int[] original = {1, 2, 3, 4};
+        int[] reverse = reverseCopy(original);
+
+        // print both
+        System.out.println( "original: " +Arrays.toString(original));
+        System.out.println( "reversed: " +Arrays.toString(reverse));
     }
     
 }
