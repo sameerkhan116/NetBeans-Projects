e4fd52f67f0a9a375a8eefd58d6518e289289b09
Sameer Khan <sameer_khan@student.uml.edu>
2017-04-11 01:31:21 -0400

    A little AI

--- 2013-OOProgrammingWithJava-PART1/week6-105.GuessingGame/src/GuessingGame.java
+++ 2013-OOProgrammingWithJava-PART1/week6-105.GuessingGame/src/GuessingGame.java
@@ -11,18 +11,38 @@ public class GuessingGame {
 
     public void play(int lowerLimit, int upperLimit) {
         instructions(lowerLimit, upperLimit);
-
-        // write the guessing logic here
+        while(!(upperLimit == lowerLimit)) {
+            if(isGreaterThan(average(lowerLimit, upperLimit))) {
+                
+                lowerLimit = average(lowerLimit, upperLimit) + 1;
+                
+            } else {
+                upperLimit = average(lowerLimit, upperLimit);
+            }
+        }
+        System.out.println("The number you are thinking of is " + upperLimit + ".");
 
     }
+    
+    public boolean isGreaterThan(int value) {
+        System.out.println("Is your number greater than " + value +"? (y/n)");
+        
+        if(this.reader.nextLine().equals("y")) {
+            return true;
+        }
+        return false;
+    }
 
+    public int average(int firstNumber, int secondNumber) {
+        return (firstNumber + secondNumber) / 2;
+    }
+    
     // implement here the methods isGreaterThan and average
 
     public void instructions(int lowerLimit, int upperLimit) {
         int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
 
         System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
-
         System.out.println("I promise you that I can guess the number you are thinking with " + maxQuestions + " questions.");
         System.out.println("");
         System.out.println("Next I'll present you a series of questions. Answer them honestly.");
--- 2013-OOProgrammingWithJava-PART1/week6-105.GuessingGame/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week6-105.GuessingGame/src/Main.java
@@ -1,8 +1,12 @@
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         // test your program here
-        GuessingGame game = new GuessingGame();
-        game.play(1,10);
+        Scanner in = new Scanner(System.in);
+        GuessingGame letsplay = new GuessingGame();
+        letsplay.play(1, 100);
     }    
 }
