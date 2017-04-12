66365733c6522fd0bd1f7e53406366264fab0787
Sameer Khan <sameer_khan@student.uml.edu>
2017-04-12 11:27:03 -0400

    Smileys done

--- 2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ 2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -2,10 +2,40 @@
 public class Smileys {
 
     public static void main(String[] args) {
-        // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+        printWithSmileys("Mikale");
     }
-
+    
+    private static void printWithSmileys(String characterString) {
+        if(characterString.length() >= 6) {
+            for(int i = 0; i < characterString.length(); i++) {
+                System.out.print(":)");
+            }
+            System.out.println("");
+            if(characterString.length() % 2 == 0) {
+                System.out.println(":) " + characterString + " :)");
+            } else {
+                System.out.println(":) " + characterString + "  :)");
+            }
+            
+            for(int i = 0; i < characterString.length(); i++) {
+                System.out.print(":)");
+            }
+        } else {
+            for(int i = 0; i <= characterString.length(); i++) {
+                System.out.print(":)");
+            }
+            System.out.println("");
+            
+            if(characterString.length() % 2 == 0) {
+                System.out.println(":) " + characterString + " :)");
+            } else {
+                System.out.println(":) " + characterString + "  :)");
+            }
+            
+            for(int i = 0; i <= characterString.length(); i++) {
+                System.out.print(":)");
+            }
+        }
+        
+    }
 }
