eb56a723488b27bb41144eefc19381629844c1ab
Sameer Khan <sameer_khan@student.uml.edu>
2017-04-13 11:38:23 -0400

    Basic HashMap program

---
+++ 2013-OOProgrammingWithJava-PART2/week7-week7_06.PromissoryNote/src/PromissoryNote.java
@@ -0,0 +1,31 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sameerkhan
+ */
+public class PromissoryNote {
+    private HashMap<String, Double> debt;
+    
+    public PromissoryNote() {
+        this.debt = new HashMap<String, Double>();
+    }
+    
+    public void setLoan(String toWhom, double value) {
+        this.debt.put(toWhom, value);
+    }
+    
+    public double howMuchIsTheDebt(String whose) {
+        if(this.debt.containsKey(whose)) {
+            return this.debt.get(whose);
+        }
+        return 0; 
+    }
+}
