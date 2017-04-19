5654b6ce4b390d99c43bb7501bff3db232f617c8
Sameer Khan <sameer_khan@student.uml.edu>
2017-04-19 11:56:14 -0400

    Jumper and Participants class added

--- 2013-OOProgrammingWithJava-PART2/week8-week8_16.SkiJumping/src/Main.java
+++ 2013-OOProgrammingWithJava-PART2/week8-week8_16.SkiJumping/src/Main.java
@@ -1,7 +1,10 @@
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        // Write your main program here. Implementing your own classes will be very useful.
+        Scanner in = new Scanner(System.in);
     }
 }
